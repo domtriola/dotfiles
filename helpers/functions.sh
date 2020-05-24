09b01e0ade45a5ec1fae1caa1ac03dac9e29f560
Dominick Triola <dominicktriola@gmail.com>
2020-05-24 14:51:23 -0700

    Add timestamp functions

--- helpers/functions.sh
+++ helpers/functions.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-# Open man page as PDF
+# pdfman opens a man page as a PDF
 function pdfman() {
  man -t "${1}" | open -f -a /Applications/Preview.app/
+}
+
+# now prints the current time in the format YEAR-MON-DAY HOUR:MIN:SEC
+function now() {
+  date +"%Y-%m-%d %H:%M:%S"
+}
+
+# timestamp prints the current Epoch time
+function timestamp() {
+  date +%s
 }
