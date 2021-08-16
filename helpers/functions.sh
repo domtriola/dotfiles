2abec2a39af19756ef9919e963da7bc2fefe8640
Dominick Triola <dominicktriola@gmail.com>
2021-08-16 15:20:42 -0700

    Create function to update maven project versions to snapshots

--- helpers/functions.sh
+++ helpers/functions.sh
@@ -26,3 +26,10 @@ function timestamp() {
 function recentmods() {
   find "$1" -type f -mtime -"$2" -exec ls -l {} \;
 }
+
+# Updates all found versions in pom.xml files to snapshot versions
+# e.g. to change <version>0.0.1</version> to <version>0.0.1-SNAPSHOT</version>:
+# mvn_snapshot 0.0.1
+function mvn_snapshot() {
+  find . -name pom.xml | xargs sed -i '' -e "s/$1/$1-SNAPSHOT/"
+}
