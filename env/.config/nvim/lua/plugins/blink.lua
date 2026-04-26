f2cd21fa571cd1fdf935650337c08b2744c47489
Dominick Triola <dominicktriola@gmail.com>
2026-04-26 09:55:38 -0700

    Disable auto completion of blink suggestions

---
+++ env/.config/nvim/lua/plugins/blink.lua
@@ -0,0 +1,16 @@
+return {
+	{
+		"saghen/blink.cmp",
+		opts = {
+      -- https://cmp.saghen.dev/configuration/completion#list
+			completion = {
+				list = {
+					selection = {
+						preselect = false,
+						auto_insert = true,
+					},
+				},
+			},
+		},
+	},
+}
