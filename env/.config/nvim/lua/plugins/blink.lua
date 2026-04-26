9b105116ecdde5c6bee7a0898dfbf83949d3d2ad
Dominick Triola <dominicktriola@gmail.com>
2026-04-26 16:14:36 -0700

    Preselect autocompletions with blink and use Tab instead of Enter to insert

--- env/.config/nvim/lua/plugins/blink.lua
+++ env/.config/nvim/lua/plugins/blink.lua
@@ -2,12 +2,18 @@ return {
 	{
 		"saghen/blink.cmp",
 		opts = {
-      -- https://cmp.saghen.dev/configuration/completion#list
+			-- https://cmp.saghen.dev/configuration/keymap
+			keymap = {
+				["<CR>"] = { "fallback" },
+				["<Tab>"] = { "select_and_accept", "fallback" },
+			},
+
+			-- https://cmp.saghen.dev/configuration/completion#list
 			completion = {
 				list = {
 					selection = {
-						preselect = false,
-						auto_insert = true,
+						preselect = true,
+						auto_insert = false,
 					},
 				},
 			},
