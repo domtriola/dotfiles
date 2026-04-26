2c094e32bca0c7028f606bdecea92dddc1426f32
Dominick Triola <dominicktriola@gmail.com>
2026-04-26 11:05:18 -0700

    Install ControlEscape and add instruction for caps lock remap on mac

---
+++ docs/manual_steps.md
@@ -0,0 +1,5 @@
+# Manual Setup Steps
+
+## Caps Lock Remap
+
+Open System Preferences, navigate to Keyboard shortcuts > Modifier Keys, and set the caps lock key to control.
--- env/.hammerspoon/init.lua
+++ env/.hammerspoon/init.lua
@@ -1,36 +1,42 @@
-local hyper = {"cmd", "alt", "ctrl"}
+local hyper = { "cmd", "alt", "ctrl" }
+
+-- ControlEscape (remaps ctrl to escape on quick press)
+hs.loadSpoon("ControlEscape"):start()
 
 -- Cherry (Pomodoro timer)
 hs.loadSpoon("Cherry")
-spoon.Cherry.duration = 25          -- minutes per session
-spoon.Cherry:bindHotkeys({})        -- uses default: cmd+ctrl+alt+C
+spoon.Cherry.duration = 25 -- minutes per session
+spoon.Cherry:bindHotkeys({}) -- uses default: cmd+ctrl+alt+C
 
 -- AppWindowSwitcher (switch between windows of specific apps)
-hs.loadSpoon("AppWindowSwitcher")
-  -- :setLogLevel("debug") -- uncomment for console debug log
-  :bindHotkeys({
-    ["Obsidian"]                  = {hyper, "1"},
-    ["iTerm"]                     = {hyper, "2"},
-    [{"com.apple.Safari",
-      "com.google.Chrome",
-      "com.kagi.kagimacOS",
-      "com.microsoft.edgemac",
-      "org.mozilla.firefox"}]     = {hyper, "3"},
-    })
+hs
+	.loadSpoon("AppWindowSwitcher")
+	-- :setLogLevel("debug") -- uncomment for console debug log
+	:bindHotkeys({
+		["Obsidian"] = { hyper, "1" },
+		["iTerm"] = { hyper, "2" },
+		[{
+			"com.apple.Safari",
+			"com.google.Chrome",
+			"com.kagi.kagimacOS",
+			"com.microsoft.edgemac",
+			"org.mozilla.firefox",
+		}] = { hyper, "3" },
+	})
 
 -- WindowHalfsAndThirds (window snapping)
 hs.loadSpoon("WindowHalfsAndThirds")
 spoon.WindowHalfsAndThirds:bindHotkeys({
-  left_half    = {hyper, "H"},
-  right_half   = {hyper, "L"},
-  top_half     = {hyper, "K"},
-  bottom_half  = {hyper, "J"},
-  top_left     = {hyper, "Y"},
-  top_right    = {hyper, "U"},
-  bottom_left  = {hyper, "B"},
-  bottom_right = {hyper, "N"},
-  max          = {hyper, "Up"},
-  third_left   = {hyper, "Left"},
-  third_right  = {hyper, "Right"},
-  center       = {hyper, "Down"},
-});
+	left_half = { hyper, "H" },
+	right_half = { hyper, "L" },
+	top_half = { hyper, "K" },
+	bottom_half = { hyper, "J" },
+	top_left = { hyper, "Y" },
+	top_right = { hyper, "U" },
+	bottom_left = { hyper, "B" },
+	bottom_right = { hyper, "N" },
+	max = { hyper, "Up" },
+	third_left = { hyper, "Left" },
+	third_right = { hyper, "Right" },
+	center = { hyper, "Down" },
+})
