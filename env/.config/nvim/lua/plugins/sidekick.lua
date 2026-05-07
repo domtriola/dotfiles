acc4c2032e3a29e388456d65b47e18f0081db8e0
Dominick Triola <dominicktriola@gmail.com>
2026-05-06 22:27:28 -0700

    Add note about disabled ai suggestions

--- env/.config/nvim/lua/plugins/sidekick.lua
+++ env/.config/nvim/lua/plugins/sidekick.lua
@@ -3,6 +3,9 @@ return {
     "folke/sidekick.nvim",
     opts = {
       nes = { -- Next Edit Suggestions
+        -- Disabled for now, so I can build a stronger intuition while learning new languages.
+        -- When copilot suggestions are desired again re-enable this for nes
+        -- and re-install the ai.copilot plugin for popup suggestions.
         enabled = false,
       },
     },
